96b2aef0a73cef58c8aa7265b6e40e0f53f5dd27
Chiraphat <ctechasiri@berkeley.edu>
2024-10-06 01:00:43 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chiraphat Techasiri on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
